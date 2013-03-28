4cdc3e0e522ac959b426a3bb1902d0a4f3afca42
happut@163.com <happut>
2013-03-28 09:50:12 +0800

    dfd

---
+++ FeiAlwaysKnowYou/src/test/java/test2.java
@@ -0,0 +1,16 @@
+/*
+ * @(#)test2.java 2013-3-28 ионГ09:49:59
+ * FeiAlwaysKnowYou
+ * Copyright 2013 Thuisoft, Inc. All rights reserved.
+ * THUNISOFT PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+/**
+ * test2
+ * @author wfei
+ * @version 1.0
+ *
+ */
+public class test2 {
+
+}
